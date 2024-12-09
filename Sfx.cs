44587a2809c6e16c3aaee5620c7d76eba7a6c109
Alex Johnson <alexfmjohnson@gmail.com>
2024-12-09 05:14:37 -0700

    Cleaned up class and added private default constructor for XMLSerializaiton

--- Sfx.cs
+++ Sfx.cs
@@ -4,6 +4,8 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace soundboard_sandbox
 {
@@ -11,46 +13,31 @@ namespace soundboard_sandbox
     // includes filepath, hotkey, length, volume, etc.
     public class Sfx
     {
-        // class vars
-        string name;
-        string hotkey;      // TODO how to store hotkey?
-        float volume;         // 0.0-1.0
-        string filePath;
-        string[] tags;
+        //// class vars
+        public string Name { get; set; }
+        public string FilePath { get; set; }         // TODO change filepath type to Path
+        public string Hotkey { get; set; }      // TODO use proper hotkey var type
+        public double Volume { get; set; }       // 0.0 - 1.0
+        // TODO public List<string> Tags { get; set; }
+        // TODO public MIDI KEY
 
-        // TODO ? create overloaded method with tags
+        // TODO create overloaded method with tags
         public Sfx(string name, string hotkey, string filePath)
         {
-            this.name = name;   // TODO CLEAN NAME
-            this.hotkey = hotkey;   // can be empty
-            this.filePath = filePath;
+            this.Name = name;
+            this.FilePath = filePath;
+            this.Hotkey = hotkey;   // can be empty
+            this.Volume = 1.0;
             //this.filePath = getFilePath(filePath);
         }
-
-        // must make accessors public for listbox to read
-        public string Name
-        { 
-            get { return this.name; } 
-        }
-        public string Hotkey
-        {
-            get { return this.hotkey; }
-        }
-        public string FilePath
-        {
-            get { return this.filePath; }
-        }
-        public string[] Tags
-        {
-            get { return this.tags; }
-        }
+        private Sfx() { } // default constructor for serialization
 
         // parse the filepath and assign it to the filePath var
-        private string getFilePath(string newFilePath)
-        {
-            // TODO exception handling
-            filePath = Path.GetFullPath(filePath);
-            return "";
-        }
+        //private string getFilePath(string newFilePath)
+        //{
+        //    // TODO exception handling
+        //    filePath = Path.GetFullPath(filePath);
+        //    return "";
+        //}
     }
 }
