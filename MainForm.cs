764cf7bf82b3e9dfcd357026443fd6afac00cc84
Alex Johnson <alexfmjohnson@gmail.com>
2024-12-11 03:21:09 -0700

    Fixed null bug

--- MainForm.cs
+++ MainForm.cs
@@ -68,17 +68,23 @@ namespace soundboard_sandbox
             using (addSfxForm addSfxForm = new addSfxForm())
             {
                 Sfx sound;
+                HotkeyEventInfo hotkeyInfo = new HotkeyEventInfo();
 
                 // Checks that 'Add' button closed the form
                 if (addSfxForm.ShowDialog() == DialogResult.OK)
                 {
+
                     // calls these methods from addSfxForm
+                    if (addSfxForm.GetHotkeyEventArgs() != null)
+                    {
+                        hotkeyInfo.KeyCode = addSfxForm.GetHotkeyEventArgs().KeyCode;
+                        hotkeyInfo.KeyData = addSfxForm.GetHotkeyEventArgs().KeyData;
+                    }
+
                     sound = new Sfx(
                         addSfxForm.GetName(),
                         addSfxForm.GetHotkeyString(),
-                        new HotkeyEventInfo(
-                            addSfxForm.GetHotkeyEventArgs().KeyCode,
-                            addSfxForm.GetHotkeyEventArgs().KeyData),
+                        hotkeyInfo,
                         addSfxForm.GetPath(),
                         addSfxForm.GetVolume()
                     );
