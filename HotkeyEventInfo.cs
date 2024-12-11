e1090b86159070749211540745c3963b108fd9c8
Alex Johnson <alexfmjohnson@gmail.com>
2024-12-11 00:14:55 -0700

    Replaced KeyEventArgs type in sfx with HotkeyEventInfo to allow for serialization (HotkeyEventInfo has a default constructor)

---
+++ HotkeyEventInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace soundboard_sandbox
+{
+    // replaces KeyEventArgs as this has a default constructor
+    public class HotkeyEventInfo
+    {
+        public Keys KeyCode { get; set;}
+        public Keys KeyData { get; set;}
+        public HotkeyEventInfo() { }
+        public HotkeyEventInfo(Keys keyCode, Keys keyData)
+        {
+            this.KeyCode = keyCode;
+            this.KeyData = keyData;
+        }
+    }
+}
