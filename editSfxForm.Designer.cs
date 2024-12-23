482fe2d498f82c24b3a2bbed09f22fad99d52e31
Alex Johnson <alexfmjohnson@gmail.com>
2024-12-22 20:49:30 -0700

    Added Edit Sfx Form

---
+++ editSfxForm.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace mini_soundboard
+{
+    partial class editSfxForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(editSfxForm));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.sfxFilePathTxt = new System.Windows.Forms.TextBox();
+            this.openFileBtn = new System.Windows.Forms.Button();
+            this.sfxNameTxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.sfxSetHotkeyBtn = new System.Windows.Forms.Button();
+            this.editSfxCancelBtn = new System.Windows.Forms.Button();
+            this.editSfxConfirmBtn = new System.Windows.Forms.Button();
+            this.sfxVolumeTrackBar = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.volumeValLbl = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.sfxVolumeTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            resources.ApplyResources(this.openFileDialog1, "openFileDialog1");
+            // 
+            // sfxFilePathTxt
+            // 
+            this.sfxFilePathTxt.AllowDrop = true;
+            resources.ApplyResources(this.sfxFilePathTxt, "sfxFilePathTxt");
+            this.sfxFilePathTxt.Name = "sfxFilePathTxt";
+            // 
+            // openFileBtn
+            // 
+            resources.ApplyResources(this.openFileBtn, "openFileBtn");
+            this.openFileBtn.Name = "openFileBtn";
+            this.openFileBtn.UseVisualStyleBackColor = true;
+            this.openFileBtn.Click += new System.EventHandler(this.getSfxPath);
+            // 
+            // sfxNameTxt
+            // 
+            resources.ApplyResources(this.sfxNameTxt, "sfxNameTxt");
+            this.sfxNameTxt.Name = "sfxNameTxt";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // sfxSetHotkeyBtn
+            // 
+            this.sfxSetHotkeyBtn.BackColor = System.Drawing.Color.White;
+            resources.ApplyResources(this.sfxSetHotkeyBtn, "sfxSetHotkeyBtn");
+            this.sfxSetHotkeyBtn.ForeColor = System.Drawing.Color.Gray;
+            this.sfxSetHotkeyBtn.Name = "sfxSetHotkeyBtn";
+            this.sfxSetHotkeyBtn.UseVisualStyleBackColor = false;
+            this.sfxSetHotkeyBtn.Enter += new System.EventHandler(this.sfxSetHotkeyBtn_Enter);
+            this.sfxSetHotkeyBtn.KeyDown += new System.Windows.Forms.KeyEventHandler(this.sfxSetHotkeyBtn_KeyDown);
+            this.sfxSetHotkeyBtn.Leave += new System.EventHandler(this.sfxSetHotkeyBtn_Leave);
+            // 
+            // editSfxCancelBtn
+            // 
+            this.editSfxCancelBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            resources.ApplyResources(this.editSfxCancelBtn, "editSfxCancelBtn");
+            this.editSfxCancelBtn.Name = "editSfxCancelBtn";
+            this.editSfxCancelBtn.UseVisualStyleBackColor = true;
+            this.editSfxCancelBtn.Click += new System.EventHandler(this.addSfxCancelBtn_Click);
+            // 
+            // editSfxConfirmBtn
+            // 
+            resources.ApplyResources(this.editSfxConfirmBtn, "editSfxConfirmBtn");
+            this.editSfxConfirmBtn.Name = "editSfxConfirmBtn";
+            this.editSfxConfirmBtn.UseVisualStyleBackColor = true;
+            this.editSfxConfirmBtn.Click += new System.EventHandler(this.addSfxConfirmBtn_Click);
+            // 
+            // sfxVolumeTrackBar
+            // 
+            resources.ApplyResources(this.sfxVolumeTrackBar, "sfxVolumeTrackBar");
+            this.sfxVolumeTrackBar.Maximum = 100;
+            this.sfxVolumeTrackBar.Name = "sfxVolumeTrackBar";
+            this.sfxVolumeTrackBar.TickFrequency = 25;
+            this.sfxVolumeTrackBar.Value = 100;
+            this.sfxVolumeTrackBar.ValueChanged += new System.EventHandler(this.sfxVolumeTrackBar_ValueChanged);
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // volumeValLbl
+            // 
+            resources.ApplyResources(this.volumeValLbl, "volumeValLbl");
+            this.volumeValLbl.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.volumeValLbl.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.volumeValLbl.Name = "volumeValLbl";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // editSfxForm
+            // 
+            this.AcceptButton = this.editSfxConfirmBtn;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.CancelButton = this.editSfxCancelBtn;
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.volumeValLbl);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.sfxVolumeTrackBar);
+            this.Controls.Add(this.editSfxConfirmBtn);
+            this.Controls.Add(this.editSfxCancelBtn);
+            this.Controls.Add(this.sfxSetHotkeyBtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sfxNameTxt);
+            this.Controls.Add(this.openFileBtn);
+            this.Controls.Add(this.sfxFilePathTxt);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "editSfxForm";
+            ((System.ComponentModel.ISupportInitialize)(this.sfxVolumeTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox sfxFilePathTxt;
+        private System.Windows.Forms.Button openFileBtn;
+        private System.Windows.Forms.TextBox sfxNameTxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button sfxSetHotkeyBtn;
+        private System.Windows.Forms.Button editSfxCancelBtn;
+        private System.Windows.Forms.Button editSfxConfirmBtn;
+        private System.Windows.Forms.TrackBar sfxVolumeTrackBar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label volumeValLbl;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
---
+++ editSfxForm.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mini_soundboard
+{
+    public partial class editSfxForm : Form
+    {
+        KeyEventArgs currentHotkey;
+
+        public editSfxForm()
+        {
+            InitializeComponent();
+        }
+
+        private void addSfxCancelBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void addSfxConfirmBtn_Click(object sender, EventArgs e)
+        {
+            // Prevent submission if path field is not filled
+            if (sfxFilePathTxt.Text == "")
+            {
+                // raise message
+                string message = $"No file path was entered.{Environment.NewLine}Would you like to exit without adding a sound effect?";
+                MessageBoxButtons yesnoBtns = MessageBoxButtons.YesNo;
+                DialogResult result;
+
+                result = MessageBox.Show(message, "Missing sfx information!", yesnoBtns);
+                if (result == System.Windows.Forms.DialogResult.Yes)
+                {
+                    // Closes the parent form.
+                    this.Close();
+                }
+            }
+            else
+            {
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+        }
+
+        // opens system file dialog window and gets selected file path
+        private void getSfxPath(object sender, EventArgs e)
+        {
+            // calls the actual dialog box
+            openFileDialog1.ShowDialog();
+
+            // once path is selected, its returned as a string
+            string path = openFileDialog1.FileName;
+            string name = Path.GetFileNameWithoutExtension(path);
+
+            if (path != null)
+            {
+                // fills text box on form
+                sfxFilePathTxt.Text = path;
+
+                // set default file name to SafeFileName
+                sfxNameTxt.Text = name;
+            }
+            // select addSfxConfirmBtn when returning to form
+            editSfxConfirmBtn.Focus();
+        }
+
+        // -- FORM GETTERS to be accessed in MainForm --
+        public string GetName() 
+        {
+            return sfxNameTxt.Text;
+        }
+
+        public string GetHotkeyString()
+        {
+            string result = "";
+
+            if (currentHotkey != null) 
+            {
+                result = Program.localHotkeys.KeysToString(currentHotkey);
+            }
+
+            return result;
+        }
+
+        public KeyEventArgs GetHotkeyEventArgs()
+        {
+            return currentHotkey;
+        }
+
+        public string GetPath()
+        {
+            if (sfxFilePathTxt.Text != null)
+                return sfxFilePathTxt.Text;
+            else 
+                return null;
+        }
+
+        public float GetVolume()
+        {
+            // NAudio expects a float value between 0.0 - 1.0
+            float sfxVolume = (float)sfxVolumeTrackBar.Value / 100;
+            Console.WriteLine(sfxVolume.GetType());
+            Console.WriteLine(sfxVolume);
+            return sfxVolume;
+        }
+
+        // reflect value from slider
+        private void sfxVolumeTrackBar_ValueChanged(object sender, EventArgs e)
+        {
+            volumeValLbl.Text = sfxVolumeTrackBar.Value.ToString();
+        }
+
+        // OnFocus
+        private void sfxSetHotkeyBtn_Enter(object sender, EventArgs e)
+        {
+            sfxSetHotkeyBtn.ForeColor = Color.Red;
+            sfxSetHotkeyBtn.Text = "Press a hotkey combination";
+        }
+        // leave focus
+        private void sfxSetHotkeyBtn_Leave(object sender, EventArgs e)
+        {
+            if (currentHotkey != null)
+            {
+                sfxSetHotkeyBtn.ForeColor = Color.Black;
+                sfxSetHotkeyBtn.Text = Program.localHotkeys.KeysToString(currentHotkey);
+            }
+            else
+            {
+                sfxSetHotkeyBtn.ForeColor = Color.Gray;
+                sfxSetHotkeyBtn.Text = "click to set Hotkey";
+            }
+        }
+
+        private void sfxSetHotkeyBtn_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.ControlKey || e.KeyCode == Keys.ShiftKey || e.KeyCode == Keys.Menu || e.KeyCode == Keys.Capital) return;
+
+            // print info on keypress
+            Console.WriteLine("Code - Data - Value");
+            Console.WriteLine($"{e.KeyCode} - {e.KeyData} - {e.KeyValue}");
+
+            // if hotkeys overlap, ask to unset old hotkey
+            Program.localHotkeys.UnsetHotkey(e.KeyData);
+
+            currentHotkey = e;
+
+            // remove focus from button
+            this.ActiveControl = null;
+        }
+    }
+}
