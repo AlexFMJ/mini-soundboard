36fd7ae4cbf6c42e6a367a3d3f242bfef68f41a6
Alex Johnson <alexfmjohnson@gmail.com>
2024-12-22 22:23:15 -0700

    Shortened HotkeyEventInfo class to HotkeyInfo

---
+++ HotkeyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mini_soundboard
+{
+    // replaces KeyEventArgs to allow for serialization with a default constructor
+    public class HotkeyInfo
+    {
+        public Keys KeyCode { get; set;}
+        public Keys KeyData { get; set;}
+        public HotkeyInfo() { }
+        public HotkeyInfo(Keys keyCode, Keys keyData)
+        {
+            this.KeyCode = keyCode;
+            this.KeyData = keyData;
+        }
+    }
+}
