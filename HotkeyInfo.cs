ba0e6dfd5460ab4bbfd0e4fe25e0666768b0a9cd
Alex Johnson <alexfmjohnson@gmail.com>
2025-01-03 20:13:49 -0700

    expanded HotKeyInfo to include midi input option

--- HotkeyInfo.cs
+++ HotkeyInfo.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NAudio.Midi;
+using System;
 using System.Windows.Forms;
 
 namespace mini_soundboard
@@ -10,8 +7,17 @@ namespace mini_soundboard
     // replaces KeyEventArgs to allow for serialization with a default constructor
     public class HotkeyInfo
     {
+        public enum HKType
+        {
+            none = 0,
+            keyboard = 1,
+            midi = 2,
+        }
+
         private string _keyString;
         private Keys _keyData;
+
+        public HKType Type { get; set; }
 
         public Keys KeyData
         {
@@ -27,10 +33,19 @@ namespace mini_soundboard
             get {  return _keyString; } 
         }
 
+        public int MidiNoteNumber { get; set; }     // number for hotkey use
+
         public HotkeyInfo() { }
         public HotkeyInfo(Keys keyData)
         {
             this.KeyData = keyData;
+            this.Type = HKType.keyboard;
+        }
+        public HotkeyInfo(NoteEvent note)
+        {
+            this.MidiNoteNumber = note.NoteNumber;
+            this._keyString = note.NoteName;
+            this.Type = HKType.midi;
         }
         
         // Sets _hotkeyString to match the keyData of this object
